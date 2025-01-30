2ad3450d635621d377306ed556b775cf2866122a
Matex2395 <mateorgafjo@gmail.com>
2025-01-30 17:07:02 -0500

    Agregar controlador de archivos (Métodos no implementados) Se ha agregado un controlador para obtener, subir y descargar archivos

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobFileUploader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpGet]
+        [Route("filename")]
+        public async Task<IActionResult> Download(string filename)
+        {
+            return Ok("Hello World");
+        }
+    }
+}
